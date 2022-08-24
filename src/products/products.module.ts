ff552f3cd3a06519b43ef6d9fbcc7138e55c7f33
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-24 13:39:24 -0300

    Global module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
-import { Module } from "@nestjs/common";
+import { /* Global,  */ Module } from "@nestjs/common";
 import { ProductsController } from "./products.controller";
 import { ProductsService } from "./products.service";
 
+//@Global() // available in all modules
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  //exports: [ProductsService], //if i want to use products in other modules
 })
 export class ProductsModule {}
