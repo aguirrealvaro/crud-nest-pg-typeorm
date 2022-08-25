ba491afe8550f3c7f7f606c461e49ecb7719dd3d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-25 17:13:58 -0300

    Improve comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { DatabaseModule } from "./database/database.module";
-import { AuthMiddleware } from "./middleware/auth.middleware";
-import { ProductsController } from "./products/products.controller";
 import { ProductsModule } from "./products/products.module";
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, ProductsModule],
 })
-export class AppModule implements NestModule {
+export class AppModule {}
+
+/* export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AuthMiddleware).forRoutes(ProductsController);
     // apply() can also accepts multiple middlewares
@@ -17,4 +17,4 @@ export class AppModule implements NestModule {
     //.forRoutes({ path: 'products', method: RequestMethod.GET })
     //.forRoutes({ path: 'products', method: RequestMethod.ALL })
   }
-}
+} */
