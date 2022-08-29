f178b0879c45aba8b4274ba53fd034346a6eeb44
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-08-29 15:46:22 -0300

    Improve comments

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 //import { ValidationPipe } from "./pipes/validation.pipe";
-// import { AuthMiddleware } from "./middleware/auth.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.use(AuthMiddleware); // global middleware
+  // global middleware, only works with a functional middleware https://docs.nestjs.com/middleware#functional-middleware
+  //app.use(AuthMiddleware);
   app.useGlobalPipes(new ValidationPipe()); //global validation pipe
   await app.listen(3000);
 }
